1fb0ae9ba2cb1a0886de8c8aff520e39c52e282c
gqcloud <874310491@qq.com>
2023-01-10 02:19:22 +0800

    test: v1-version

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {},
+  prompt: {
+    useEmoji: true,
+    emojiAlign: "center",
+    allowCustomIssuePrefixs: false,
+    allowEmptyIssuePrefixs: false
+  }
+}
